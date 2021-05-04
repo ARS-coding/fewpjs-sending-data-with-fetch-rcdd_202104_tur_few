4fe9d361b44814c20ed9b7228144fd28532f4d8b
Ali Rıza <ars.lifestyle@hotmail.com>
2021-05-04 11:53:12 +0300

    commit tree e420edf7eac0fd73847cc509d58fd61489be3950

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+
+function submitData(name, email) {
+    let url = "http://localhost:3000/profile";
+    let data = {
+        name: name,
+        email: email
+    };
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(data) 
+    };
+    return fetch(url, configObj)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(json) {
+        console.log(json);  
+    })
+    .catch()
+}
+
+submitData("li", "iza");
